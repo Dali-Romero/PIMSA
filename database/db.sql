caebae18d8a26ec9371efeb831ecd18b99fe1974
Hagantyr <kevinaherrerae@gmail.com>
2023-07-26 16:16:34 -0600

    [Cambio] Traduccion al espeñol de la base de datos

--- database/db.sql
+++ database/db.sql
@@ -1,116 +1,124 @@
+/*
+Nomenclatura:
+    camel case (camelCase): llaves primaria y campos de la misma tabla.
+    snake case (snake_case): llaves foraneas.
+    pascal case (PascalCase): nombres de tablas.
+*/
+
 -- creacion y uso de la base de datos
-CREATE DATABASE IF NOT EXISTS pimsa_db;
-USE pimsa_db;
+CREATE DATABASE IF NOT EXISTS pimsa_db_prueba;
+USE pimsa_db_prueba;
 
 -- creacion de tablas
-CREATE TABLE users(
-    userId INT NOT NULL AUTO_INCREMENT,
-    name CHAR(25) NOT NULL,
-    lName CHAR(25) NOT NULL,
-    eMail CHAR(40) NOT NULL,
-    pass BINARY(60) NOT NULL,
-    ejecutivo_id INT, -- llave foranea
+CREATE TABLE Usuarios(
+    usuarioId INT NOT NULL AUTO_INCREMENT,
+    nombre CHAR(25) NOT NULL,
+    apellido CHAR(25) NOT NULL,
+    correoElec CHAR(40) NOT NULL,
+    contrasena BINARY(60) NOT NULL,
+    empleado_id INT, -- llave foranea
     rol_id INT, -- llave foranea
-    PRIMARY KEY(userId)
+    PRIMARY KEY(usuarioId)
 );
 
-CREATE TABLE empleados(
-    ejecutivoId INT NOT NULL AUTO_INCREMENT,
-    fullName CHAR(50) NOT NULL,
-    eMail CHAR(40) NOT NULL,
+CREATE TABLE Empleados(
+    empleadoId INT NOT NULL AUTO_INCREMENT,
+    nombreComp CHAR(50) NOT NULL,
+    correoElec CHAR(40) NOT NULL,
     rol_id INT, -- llave foranea
     numeroNomina INT NOT NULL,
     salarioQuin INT NOT NULL,
     compleNomina CHAR(20) NOT NULL,
     fechaIngreso DATE NOT NULL,
-    anoPorCumplir INT NOT NULL,
+    anosCumplir INT NOT NULL,
     area_id INT, -- llave foranea
-    cellphone CHAR(10) NOT NULL,
-    emerName CHAR(50) NOT NULL,
-    emerCell CHAR(10) NOT NULL,
+    numeroCelu CHAR(10) NOT NULL,
+    emerNombre CHAR(50) NOT NULL,
+    emerCelu CHAR(10) NOT NULL,
     estadoCivil CHAR(12) NOT NULL,
     curp CHAR(18) NOT NULL,
     rfc CHAR(13) NOT NULL,
     sexo CHAR(1) NOT NULL,
     nacLugar CHAR(30) NOT NULL,
-    nacDate DATE NOT NULL,
+    nacFecha DATE NOT NULL,
     edadCumplir INT NOT NULL,
     domicilio CHAR(60) NOT NULL,
     nss CHAR(11) NOT NULL,
-    registroPatr CHAR(60) NOT NULL,
+    registroPatro CHAR(60) NOT NULL,
     estatus CHAR(20) NOT NULL,
     inicioContrato DATE NOT NULL,
-    finContraro DATE NOT NULL,
-    PRIMARY KEY(ejecutivoId)
+    finContrato DATE NOT NULL,
+    PRIMARY KEY(empleadoId)
 );
 
--- historial_empleado table
-CREATE TABLE historialEmpleados(
+CREATE TABLE HistorialEmpleados(
     cambioId INT NOT NULL AUTO_INCREMENT,
-    modificado_user_id INT, -- llave foranea
-    ejecutivo_id INT, -- llave foranea
+    modificado_usuario_id INT, -- llave foranea
+    empleado_id INT, -- llave foranea
     cambioRealizado CHAR(100) NOT NULL,
-    fechaCambio DATE NOT NULL,
+    fechaCambio TIMESTAMP NOT NULL DEFAULT current_timestamp,
     --
-    fullName CHAR(50) NOT NULL,
-    eMail CHAR(40) NOT NULL,
+    nombreComp CHAR(50) NOT NULL,
+    correoElec CHAR(40) NOT NULL,
     rol_id INT, -- llave foranea
     numeroNomina INT NOT NULL,
     salarioQuin INT NOT NULL,
     compleNomina CHAR(20) NOT NULL,
     fechaIngreso DATE NOT NULL,
-    anoPorCumplir INT NOT NULL,
+    anosCumplir INT NOT NULL,
     area_id INT, -- llave foranea
-    cellphone CHAR(10) NOT NULL,
-    emerName CHAR(50) NOT NULL,
-    emerCell CHAR(10) NOT NULL,
+    numeroCelu CHAR(10) NOT NULL,
+    emerNombre CHAR(50) NOT NULL,
+    emerCelu CHAR(10) NOT NULL,
     estadoCivil CHAR(12) NOT NULL,
     curp CHAR(18) NOT NULL,
     rfc CHAR(13) NOT NULL,
     sexo CHAR(1) NOT NULL,
     nacLugar CHAR(30) NOT NULL,
-    nacDate DATE NOT NULL,
+    nacFecha DATE NOT NULL,
     edadCumplir INT NOT NULL,
     domicilio CHAR(60) NOT NULL,
     nss CHAR(11) NOT NULL,
-    registroPatr CHAR(60) NOT NULL,
+    registroPatro CHAR(60) NOT NULL,
     estatus CHAR(20) NOT NULL,
     inicioContrato DATE NOT NULL,
-    finContraro DATE NOT NULL,
+    finContrato DATE NOT NULL,
     PRIMARY KEY(cambioId)
 );
 
-CREATE TABLE permisos(
-    permisosId INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE Permisos(
+    permisoId INT NOT NULL AUTO_INCREMENT,
     descripcion CHAR(20) NOT NULL,
-    PRIMARY KEY(permisosId)
+    PRIMARY KEY(permisoId)
 );
 
-CREATE TABLE permisosRol(
-    permisos_id INT, -- llave foranea
+-- Tabla pivote
+CREATE TABLE PermisosRoles(
+    permiso_id INT, -- llave foranea
     rol_id INT -- llave foranea
 );
 
-CREATE TABLE rol(
+CREATE TABLE Roles(
     rolId INT NOT NULL AUTO_INCREMENT,
-    name CHAR(20) NOT NULL,
-    active BOOLEAN NOT NULL,
+    nombre CHAR(20) NOT NULL,
+    activo BOOLEAN NOT NULL,
     PRIMARY KEY(rolId)
 );
 
-CREATE TABLE areasRoles(
+-- Tabla pivote
+CREATE TABLE AreasRoles(
     rol_id INT, -- llave foranea
     area_id INT -- llave foranea
 );
 
-CREATE TABLE areas(
+CREATE TABLE Areas(
     areaId INT NOT NULL AUTO_INCREMENT,
     nombre CHAR(30) NOT NULL,
-    active BOOLEAN NOT NULL,
+    activo BOOLEAN NOT NULL,
     PRIMARY KEY(areaId)
 );
 
-CREATE TABLE maquinas(
+CREATE TABLE Maquinas(
     maquinaId INT NOT NULL AUTO_INCREMENT,
     nombre CHAR(20) NOT NULL,
     descripcion CHAR(40) NOT NULL,
@@ -119,34 +127,35 @@ CREATE TABLE maquinas(
     PRIMARY KEY(maquinaId)
 );
 
-CREATE TABLE maquinasUser(
-    user_id INT, -- llave foranea
+-- Tabla pivote
+CREATE TABLE MaquinasUsuarios(
+    usuario_id INT, -- llave foranea
     maquina_id INT -- llave foranea
 );
 
-CREATE TABLE outOfCatalogCotizado(
+CREATE TABLE FueraCatalogoCotizados(
     cot_id INT, -- llave foranea
     cantidad INT NOT NULL,
     concepto CHAR(50) NOT NULL,
-    acabados CHAR(20) NOT NULL,
-    archivo CHAR(20) NOT NULL,
+    acabados CHAR(20) DEFAULT 'NULO',
+    archivo CHAR(20) DEFAULT 'NULO',
     precio INT NOT NULL,
-    medidaAlt INT NOT NULL,
-    medidaAnch INT NOT NULL
+    medidaAlto INT NOT NULL,
+    medidaAncho INT NOT NULL
 );
 
-CREATE TABLE orden(
+CREATE TABLE Ordenes(
     ordenId INT NOT NULL AUTO_INCREMENT,
-    genDate DATE NOT NULL,
-    deliveryDate DATE NOT NULL,
+    fechaGen TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    fechaEnt DATE NOT NULL,
     cot_id INT, -- llave foranea
-    user_id INT, -- llave foranea
+    usuario_id INT, -- llave foranea
     area_id INT, -- llave foranea
-    finished BOOLEAN NOT NULL,
+    terminada BOOLEAN NOT NULL,
     PRIMARY KEY(ordenId)
 );
 
-CREATE TABLE cobranza(
+CREATE TABLE Cobranza(
     cobranzaId INT NOT NULL AUTO_INCREMENT,
     orden_id INT, -- llave foranea
     tipoPago CHAR(10) NOT NULL,
@@ -155,155 +164,160 @@ CREATE TABLE cobranza(
     PRIMARY KEY(cobranzaId)
 );
 
-CREATE TABLE tareas(
+CREATE TABLE Tareas(
     tareaId INT NOT NULL AUTO_INCREMENT,
     orden_id INT, -- llave foranea
-    user_id INT, -- llave foranea
-    areaAct_id INT, -- llave foranea
+    usuario_id INT, -- llave foranea
+    area_id INT, -- llave foranea
     maquina_id INT, -- llave foranea
-    finished BOOLEAN NOT NULL,
+    terminada BOOLEAN NOT NULL,
     PRIMARY KEY(tareaId)
 );
 
-CREATE TABLE grupos(
+CREATE TABLE Grupos(
     grupoId INT NOT NULL AUTO_INCREMENT,
     nombre CHAR(20) NOT NULL,
     PRIMARY KEY(grupoId)
 );
 
-CREATE TABLE cotizados(
+CREATE TABLE Cotizaciones(
     cotId INT NOT NULL AUTO_INCREMENT,
-    fecha DATE NOT NULL,
-    client_id INT, -- llave foranea
+    fecha TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    cliente_id INT, -- llave foranea
     descuento INT NOT NULL,
     proyecto CHAR(30) NOT NULL,
     observaciones CHAR(100) NOT NULL,
     PRIMARY KEY (cotId)
 );
 
-CREATE TABLE productoCotizado(
+CREATE TABLE ProductosCotizados(
     prodCotizadoId INT NOT NULL AUTO_INCREMENT,
     cot_id INT, -- llave foranea
     cantidad INT NOT NULL,
-    codigo_id INT, -- llave foranea
-    acabados CHAR(20) NOT NULL,
-    archivos CHAR(20) NOT NULL,
-    precioMt2 INT NOT NULL,
+    producto_id INT, -- llave foranea
+    acabados CHAR(20) DEFAULT 'NULO',
+    archivo CHAR(20) DEFAULT 'NULO',
+    precioMtCuad INT NOT NULL,
     precioPieza INT NOT NULL,
-    medidaAlt INT NOT NULL,
-    medidaAnch INT NOT NULL,
+    medidaAlto INT NOT NULL,
+    medidaAncho INT NOT NULL,
     PRIMARY KEY (prodCotizadoId)
 );
 
-CREATE TABLE procesosOrden(
+-- Tabla pivote
+CREATE TABLE ProcesosOrdenes(
     area_id INT, -- llave foranea
     proceso_id INT, -- llave foranea
     orden INT NOT NULL
 );
 
-CREATE TABLE procesos(
+CREATE TABLE Procesos(
     procesoId INT NOT NULL AUTO_INCREMENT,
     nombre CHAR(30) NOT NULL,
     PRIMARY KEY(procesoId)
 );
 
-CREATE TABLE clients(
-    clientId INT NOT NULL AUTO_INCREMENT,
-    name CHAR(50) NOT NULL,
-    user_id INT, -- llave foranea
-    group_id INT, -- llave foranea
-    contact CHAR(40) NOT NULL,
+CREATE TABLE Clientes(
+    clienteId INT NOT NULL AUTO_INCREMENT,
+    nombre CHAR(50) NOT NULL,
+    usuario_id INT, -- llave foranea
+    grupo_id INT, -- llave foranea
+    contacto CHAR(40) NOT NULL,
     razonSocial CHAR(50) NOT NULL,
     rfc CHAR(13) NOT NULL,
     domCalle CHAR(30) NOT NULL,
     domNumEx CHAR(5) NOT NULL,
-    domNumIn CHAR(5),
+    domNumIn CHAR(5) DEFAULT 'NULO',
     domColonia CHAR(30) NOT NULL,
     domCp INT NOT NULL,
     domEstado CHAR(20) NOT NULL,
     domCiudad CHAR(30) NOT NULL,
-    telefono INT NOT NULL,
-    telefonoExt INT,
-    celular INT NOT NULL,
-    eMail CHAR(40) NOT NULL,
-    eMailAlt CHAR(40),
+    telefono CHAR(10) NOT NULL,
+    telefonoExt CHAR(10) DEFAULT 'NULO',
+    celular CHAR(10) NOT NULL,
+    correoElec CHAR(40) NOT NULL,
+    correoElecAlt CHAR(40) DEFAULT 'NULO',
     limiteCredito INT NOT NULL,
     diasCredito INT NOT NULL,
     descuento INT NOT NULL,
     observaciones CHAR(200) NOT NULL,
-    active BOOLEAN NOT NULL,
-    PRIMARY KEY(clientId)
+    activo BOOLEAN NOT NULL,
+    PRIMARY KEY(clienteId)
 );
 
-CREATE TABLE producto(
-    codigoId INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE Productos(
+    productoId INT NOT NULL AUTO_INCREMENT,
     nombre CHAR(30) NOT NULL,
     descripcion CHAR(100) NOT NULL,
     unidad CHAR(10) NOT NULL,
     precio INT NOT NULL,
-    applyDiscount BOOLEAN NOT NULL,
+    aplicarDescuento BOOLEAN NOT NULL,
     proceso_id INT, -- llave foranea
-    PRIMARY KEY(codigoId)
+    PRIMARY KEY(productoId)
 );
 
 -- asignacion de llaves foraneas
-ALTER TABLE users
-    ADD CONSTRAINT fk_users_empleados FOREIGN KEY (ejecutivo_id) REFERENCES empleados(ejecutivoId),
-    ADD CONSTRAINT fk_users_rol FOREIGN KEY (rol_id) REFERENCES rol(rolId);
+ALTER TABLE Usuarios
+    ADD CONSTRAINT fk_usuarios_empleados FOREIGN KEY (empleado_id) REFERENCES Empleados(empleadoId),
+    ADD CONSTRAINT fk_usuarios_roles FOREIGN KEY (rol_id) REFERENCES Roles(rolId);
 
-ALTER TABLE empleados
-    ADD CONSTRAINT fk_empleados_rol FOREIGN KEY (rol_id) REFERENCES rol(rolId),
-    ADD CONSTRAINT fk_empleados_areas FOREIGN KEY (area_id) REFERENCES areas(areaId);
+ALTER TABLE Empleados
+    ADD CONSTRAINT fk_empleados_roles FOREIGN KEY (rol_id) REFERENCES Roles(rolId),
+    ADD CONSTRAINT fk_empleados_areas FOREIGN KEY (area_id) REFERENCES Areas(areaId);
 
-ALTER TABLE historialEmpleados
-    ADD CONSTRAINT fk_historial_users FOREIGN KEY (modificado_user_id) REFERENCES users(userId),
-    ADD CONSTRAINT fk_historial_empleados FOREIGN KEY (ejecutivo_id) REFERENCES empleados(ejecutivoId),
-    ADD CONSTRAINT fk_historial_rol FOREIGN KEY (rol_id) REFERENCES rol(rolId),
-    ADD CONSTRAINT fk_historial_area FOREIGN KEY (area_id) REFERENCES areas(areaId);
+ALTER TABLE HistorialEmpleados
+    ADD CONSTRAINT fk_historial_usuarios FOREIGN KEY (modificado_usuario_id) REFERENCES Usuarios(usuarioId),
+    ADD CONSTRAINT fk_historial_empleados FOREIGN KEY (empleado_id) REFERENCES Empleados(empleadoId),
+    ADD CONSTRAINT fk_historial_roles FOREIGN KEY (rol_id) REFERENCES Roles(rolId),
+    ADD CONSTRAINT fk_historial_areas FOREIGN KEY (area_id) REFERENCES Areas(areaId);
 
-ALTER TABLE permisosRol
-    ADD CONSTRAINT fk_permisosRol_permisos FOREIGN KEY (permisos_id) REFERENCES permisos(permisosId),
-    ADD CONSTRAINT fk_permisosRol_rol FOREIGN KEY (rol_id) REFERENCES rol(rolId);
+-- Tabla pivote
+ALTER TABLE PermisosRoles
+    ADD CONSTRAINT fk_permisosRoles_permisos FOREIGN KEY (permiso_id) REFERENCES Permisos(permisoId),
+    ADD CONSTRAINT fk_permisosRoles_roles FOREIGN KEY (rol_id) REFERENCES Roles(rolId);
 
-ALTER TABLE areasRoles
-    ADD CONSTRAINT fk_areasRoles_rol FOREIGN KEY (rol_id) REFERENCES rol(rolId),
-    ADD CONSTRAINT fk_areasRoles_area FOREIGN KEY (area_id) REFERENCES areas(areaId);
+-- Tabla pivote
+ALTER TABLE AreasRoles
+    ADD CONSTRAINT fk_areasRoles_roles FOREIGN KEY (rol_id) REFERENCES Roles(rolId),
+    ADD CONSTRAINT fk_areasRoles_areas FOREIGN KEY (area_id) REFERENCES Areas(areaId);
 
-ALTER TABLE maquinasUser
-    ADD CONSTRAINT fk_maquinasUser_user FOREIGN KEY (user_id) REFERENCES users(userId),
-    ADD CONSTRAINT fk_maquinasUser_maquina FOREIGN KEY (maquina_id) REFERENCES maquinas(maquinaId);
+-- Tabla pivote
+ALTER TABLE MaquinasUsuarios
+    ADD CONSTRAINT fk_maquinasUsuarios_usuarios FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuarioId),
+    ADD CONSTRAINT fk_maquinasUsuarios_maquinas FOREIGN KEY (maquina_id) REFERENCES Maquinas(maquinaId);
 
-ALTER TABLE outOfCatalogCotizado
-    ADD CONSTRAINT fk_outCatalogo_cotizacion FOREIGN KEY (cot_id) REFERENCES cotizados(cotId);
+ALTER TABLE FueraCatalogoCotizados
+    ADD CONSTRAINT fk_fuera_cotizaciones FOREIGN KEY (cot_id) REFERENCES Cotizaciones(cotId);
 
-ALTER TABLE orden
-    ADD CONSTRAINT fk_orden_cotizacion FOREIGN KEY (cot_id) REFERENCES cotizados(cotId),
-    ADD CONSTRAINT fk_orden_user FOREIGN KEY (user_id) REFERENCES users(userId),
-    ADD CONSTRAINT fk_orden_area FOREIGN KEY (area_id) REFERENCES areas(areaId);
+ALTER TABLE Ordenes
+    ADD CONSTRAINT fk_ordenes_cotizaciones FOREIGN KEY (cot_id) REFERENCES Cotizaciones(cotId),
+    ADD CONSTRAINT fk_ordenes_usuarios FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuarioId),
+    ADD CONSTRAINT fk_ordenes_areas FOREIGN KEY (area_id) REFERENCES Areas(areaId);
 
-ALTER TABLE cobranza
-    ADD CONSTRAINT fk_cobranza_orden FOREIGN KEY (orden_id) REFERENCES orden(ordenId);
+ALTER TABLE Cobranza
+    ADD CONSTRAINT fk_cobranza_ordenes FOREIGN KEY (orden_id) REFERENCES Ordenes(ordenId);
 
-ALTER TABLE tareas
-    ADD CONSTRAINT fk_tareas_orden FOREIGN KEY (orden_id) REFERENCES orden(ordenId),
-    ADD CONSTRAINT fk_tareas_user FOREIGN KEY (user_id) REFERENCES users(userId),
-    ADD CONSTRAINT fk_tareas_areaAct FOREIGN KEY (areaAct_id) REFERENCES areas(areaId),
-    ADD CONSTRAINT fk_tareas_maquina FOREIGN KEY (maquina_id) REFERENCES maquinas(maquinaId);
+ALTER TABLE Tareas
+    ADD CONSTRAINT fk_tareas_ordenes FOREIGN KEY (orden_id) REFERENCES Ordenes(ordenId),
+    ADD CONSTRAINT fk_tareas_usuarios FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuarioId),
+    ADD CONSTRAINT fk_tareas_areas FOREIGN KEY (area_id) REFERENCES Areas(areaId),
+    ADD CONSTRAINT fk_tareas_maquinas FOREIGN KEY (maquina_id) REFERENCES Maquinas(maquinaId);
 
-ALTER TABLE cotizados
-    ADD CONSTRAINT fk_cotizados_client FOREIGN KEY (client_id) REFERENCES clients(clientId);
+ALTER TABLE Cotizaciones
+    ADD CONSTRAINT fk_cotizaciones_clientes FOREIGN KEY (cliente_id) REFERENCES Clientes(clienteId);
 
-ALTER TABLE productoCotizado
-    ADD CONSTRAINT fk_productoCotizado_cotizacion FOREIGN KEY (cot_id) REFERENCES cotizados(cotId),
-    ADD CONSTRAINT fk_productoCotizado_producto FOREIGN KEY (codigo_id) REFERENCES producto(codigoId);
+ALTER TABLE ProductosCotizados
+    ADD CONSTRAINT fk_productosCotizados_cotizaciones FOREIGN KEY (cot_id) REFERENCES Cotizaciones(cotId),
+    ADD CONSTRAINT fk_productosCotizados_productos FOREIGN KEY (producto_id) REFERENCES Productos(productoId);
 
-ALTER TABLE procesosOrden
-    ADD CONSTRAINT fk_procesosOrden_area FOREIGN KEY (area_id) REFERENCES areas(areaId),
-    ADD CONSTRAINT fk_procesosOrden_proceso FOREIGN KEY (proceso_id) REFERENCES procesos(procesoId);
+-- Tabla pivote
+ALTER TABLE ProcesosOrdenes
+    ADD CONSTRAINT fk_procesosOrdenes_areas FOREIGN KEY (area_id) REFERENCES Areas(areaId),
+    ADD CONSTRAINT fk_procesosOrdenes_procesos FOREIGN KEY (proceso_id) REFERENCES Procesos(procesoId);
 
-ALTER TABLE clients
-    ADD CONSTRAINT fk_clients_user FOREIGN KEY (user_id) REFERENCES users(userId),
-    ADD CONSTRAINT fk_clients_group FOREIGN KEY (group_id) REFERENCES grupos(grupoId);
+ALTER TABLE Clientes
+    ADD CONSTRAINT fk_clientes_usuarios FOREIGN KEY (usuario_id) REFERENCES Usuarios(usuarioId),
+    ADD CONSTRAINT fk_clientes_grupos FOREIGN KEY (grupo_id) REFERENCES Grupos(grupoId);
 
-ALTER TABLE producto
-    ADD CONSTRAINT fk_producto_proceso FOREIGN KEY (proceso_id) REFERENCES procesos(procesoId);
+ALTER TABLE Productos
+    ADD CONSTRAINT fk_productos_procesos FOREIGN KEY (proceso_id) REFERENCES Procesos(procesoId);
