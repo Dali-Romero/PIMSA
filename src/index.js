f153cde5fd5c37d34916ab8cd323d5106af80986
Hagantyr <kevinaherrerae@gmail.com>
2023-07-27 13:06:09 -0600

    [Creacion] configuracion de la conexion con la base de datos

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const {engine} = require('express-handlebars');
 const path = require('path');
+const flash = require('connect-flash');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const {database} = require('./keys.js');
+const passport = require('passport');
 
 // Initializations
 const app = express();
@@ -19,9 +24,18 @@ app.engine('.hbs', engine({
 app.set('view engine', '.hbs');
 
 // Middlewares
+app.use(session({
+    secret: 'mysqlSession',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore(database)
+}));
+app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Global variables
 app.use((req, res, next)=>{
@@ -30,6 +44,7 @@ app.use((req, res, next)=>{
 
 // Routes
 app.use(require('./routes/index.js'));
+app.use('/machines', require('./routes/machines.js'));
 
 // Public
 app.use(express.static(path.join(__dirname, 'public')));
