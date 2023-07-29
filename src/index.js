fc37b3875e8801ee31baece6bddbf9f0564decfa
Dali-Romero <137463346+Dali-Romero@users.noreply.github.com>
2023-07-29 12:26:19 -0600

    [FEATURE] Login
    Creacion de login funcional y rutas. No cambie keys.js.

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const passport = require('passport');
 
 // Initializations
 const app = express();
+require('./lib/passport.js');
 
 // Settings
 app.set('port', process.env.PORT || 4000);
@@ -36,15 +37,21 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
 // Global variables
 app.use((req, res, next)=>{
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    res.locals.user = req.user;
     next();
 });
 
+
 // Routes
-app.use(require('./routes/index.js'));
-app.use('/machines', require('./routes/machines.js'));
+app.use(require('./routes/index'));
+app.use(require('./routes/login'));
 
 // Public
 app.use(express.static(path.join(__dirname, 'public')));
---
+++ src/lib/passport.js
@@ -0,0 +1,34 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+
+const pool = require('../database');
+const helpers = require('./helpers');
+
+passport.use('local.login', new localStrategy({
+    usernameField: 'correo',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async (req, correo, password, done) => {
+    const rows = await pool.query('SELECT * FROM usuarios WHERE correoElec = ?', [correo]);
+    if (rows.length > 0){
+        const user = rows[0];
+        const validPass = await helpers.matchPassword(password, user.contrasena);
+        if (validPass){
+            done(null, user, req.flash('success_msg', 'Bienvenido(a) ' + user.nombre + " " + user.apellido));
+        } else{
+            done(null, false, req.flash('error_msg', 'Contraseña incorrecta...'));
+        }
+    } else{
+        done(null, null, req.flash('error_msg', 'El usuario no existe...'));
+    }
+}));
+
+
+passport.serializeUser((user, done) => {
+    done(null, user.usuarioId);
+});
+
+passport.deserializeUser( async (usuarioId, done) => {
+    const rows = await pool.query('SELECT * FROM usuarios WHERE usuarioId = ?', [usuarioId]);
+    done(null, rows[0]);
+});
