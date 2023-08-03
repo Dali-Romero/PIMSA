49d1bc21ad1e9c19f200deb30d3429310ce5a72b
Dali-Romero <137463346+Dali-Romero@users.noreply.github.com>
2023-08-03 16:39:58 -0600

    [FEATURE] Pantallas funcionales
    Todo funcional, hace falta terminar los filtros para que funcionen perfecto y hay un pequeño error cuando se va el formulario con 0

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -1,3 +1,11 @@
-const helpers = {}
+const hbs = require('handlebars');
 
-module.exports = helpers;
+hbs.registerHelper('equal', function(v1, v2, options){
+    if (v1 === v2){
+        console.log(v1 +' es igual a '+v2);
+        return options.fn(this);
+    } else{
+        console.log(v1 + ' es diferente a ' + v2);
+        return options.inverse(this);
+    }
+});
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,12 +3,79 @@ const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
 const router = express.Router();
 
-router.get('/', isLoggedIn, (req, res) =>{
-    res.render('../views/products/allProducts');
+router.get('/', isLoggedIn, async (req, res) =>{
+    const products = await pool.query('SELECT * FROM productos');
+    const procesos = await pool.query('SELECT * FROM procesos');
+    res.render('../views/products/allProducts', {products, procesos});
 });
 
-router.get('/add', isLoggedIn, (req, res) =>{
-    res.render('../views/products/addProduct');
+router.post('/', isLoggedIn, async (req, res) =>{
+    const {buscar, productSearchField} = req.body;
+    var products = [];
+    if (buscar == '' || buscar == null){
+        products = await pool.query('SELECT * FROM productos');
+    } else if (productSearchField == 'nombre'){
+        products = await pool.query('SELECT * FROM productos WHERE nombre = ?',[buscar]);
+    } else if (productSearchField == 'unidad'){
+        products = await pool.query('SELECT * FROM productos WHERE unidad = ?',[buscar]);
+    }
+    
+    res.render('../views/products/allProducts', {products}); 
 });
+
+router.get('/add', isLoggedIn, async (req, res) =>{
+    const procesos = await pool.query('SELECT * FROM procesos');
+    console.log(procesos[0].nombre);
+    res.render('../views/products/addProduct', {procesos});
+});
+
+router.post('/add', isLoggedIn, async (req, res) =>{
+    const product = req.body;
+    const newProduct = {
+        nombre: product.nombre,
+        descripcion: product.descripcion,
+        unidad: product.unidadMedida,
+        precio: product.precio,
+        aplicarDescuento: product.descuento,
+        porcentaje: product.porcentaje,
+        proceso_id: product.proceso
+    };
+    pool.query('INSERT into productos SET ?', [newProduct]);
+    req.flash('success_msg', 'Producto agregado correctamente.');
+    res.redirect('/products');
+
+});
+
+router.get('/edit/:id', isLoggedIn, async (req, res) =>{
+    const {id} = req.params;
+    const product = await pool.query('SELECT * FROM productos WHERE productoId = ?', [id]);
+    const editProduct = {
+        proceso: await pool.query('SELECT * FROM procesos'),
+        proceso_id: product[0].proceso_id
+    }
+    console.log(editProduct);
+    res.render('products/editProduct', {product: product[0], editProduct});
+
+});
+
+router.post('/edit/:id', isLoggedIn, async (req, res) =>{
+    const {id} = req.params;
+    const product = req.body;
+    const editProduct = {
+        nombre: product.nombre,
+        descripcion: product.descripcion,
+        unidad: product.unidadMedida,
+        precio: product.precio,
+        aplicarDescuento: product.descuento,
+        porcentaje: product.porcentaje,
+        proceso_id: product.proceso
+    };
+    console.log('Porcentaje: ' + editProduct.porcentaje)
+    pool.query('UPDATE productos SET ? WHERE productoId = ?', [editProduct, id]);
+    req.flash('success_msg', 'Producto modificado correctamente.');
+    res.redirect('/products');
+
+});
+
 
 module.exports = router;
