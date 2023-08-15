4983cb1fd3ff4f1f4060a4c731187c44273a9c6d
Dali-Romero <137463346+Dali-Romero@users.noreply.github.com>
2023-08-14 21:40:55 -0600

    [USERS] Formulario funcional
    El formulario de crear nuevo usuario funciona bien.

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,22 +1,48 @@
 const express = require('express');
 const pool = require('../database');
 const { isLoggedIn } = require('../lib/auth');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
+
 
 router.get('/', isLoggedIn, async (req, res) =>{
     const users = await pool.query('SELECT * FROM usuarios');
-    res.render('../views/users/allUsers', {users});
+    const roles = await pool.query('SELECT * FROM roles');
+    const empleados = await pool.query('SELECT * FROM empleados');
+    console.log(roles);
+    res.render('../views/users/allUsers', {users, roles, empleados});
 });
 
-router.get('/add', isLoggedIn, (req, res) =>{
-    res.render('../views/users/newUser');
+router.get('/add', isLoggedIn, async (req, res) =>{
+    const roles = await pool.query('SELECT * FROM roles WHERE activo = 1');
+    const empleados = await pool.query('SELECT * FROM empleados');
+    res.render('../views/users/newUser', {roles, empleados});
+});
+
+router.post('/add', isLoggedIn, async (req, res)=>{
+    const user = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.pass, salt);
+    const newUser = {
+        nombre: user.nombre,
+        apellido: user.apellido,
+        correoElec: user.correo,
+        activo: user.status,
+        Empleado_id: user.empleado,
+        Rol_id: user.rol,
+        Contrasena: hash
+    };
+    pool.query('INSERT INTO usuarios SET ?', [newUser]);
+    req.flash('success', 'Usuario registrado con exito.');
+    res.redirect('/users');
 });
 
 router.get('/edit/:id', isLoggedIn, async(req, res) =>{
     const {id} = req.params;
     const users = await pool.query('SELECT * FROM usuarios WHERE usuarioId = ?', [id]);
-    const user = users[0];
-    res.render('../views/users/editUser', {user});
+    const roles = await pool.query('SELECT * FROM roles WHERE activo = 1');
+    const empleados = await pool.query('SELECT * FROM empleados');
+    res.render('../views/users/editUser', {user: users[0]}, roles, empleados);
 });
 
 module.exports = router;
