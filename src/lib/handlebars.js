87b675598ce628c8375d36255984e4fce6ecbe57
Hagantyr <kevinaherrerae@gmail.com>
2023-09-11 16:49:51 -0600

    [Adjustment] Se ajusto el formato de los helpers

--- src/lib/handlebars.js
+++ src/lib/handlebars.js
@@ -16,29 +16,26 @@ hbs.registerHelper('equalNR', function(v1, v2, options){
     }
 });
 
-const helpers = {};
-
-helpers.formatNumber = (n) => {
+hbs.registerHelper('formatNumber', function(n){
     return Number(n).toLocaleString();
-};
+});
 
-helpers.formatNumberMeasure = (n) => {
+hbs.registerHelper('formatNumberMeasure', function(n){
     if(Number.isInteger(n)){
         return Number(n)
     }else{
         return Number(Number.parseFloat(n).toFixed(3));
     }
-};
+});
 
-helpers.formatDate = (date) => {
+hbs.registerHelper('formatDate', function(date){
     date = new Date(date);
     fecha = date.toLocaleDateString('es-mx', {year: 'numeric', month: 'long', day: 'numeric', hour:'numeric', minute: 'numeric'});
     return fecha;
-};
-helpers.formatDateShort = (date) => {
+});
+
+hbs.registerHelper('formatDateShort', function(date){
     date = new Date(date);
     fecha = date.toLocaleDateString('es-mx', {year: 'numeric', month: '2-digit', day: '2-digit', hour:'numeric', minute: 'numeric'});
     return fecha;
-};
-
-module.exports = helpers;
+});
