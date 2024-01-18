5b6b44081a7f7595b7c40ebe2d29e2cfb8d608fd
Hagantyr <kevinaherrerae@gmail.com>
2024-01-17 18:51:02 -0600

    [Feature] Registrar procesos

--- src/public/js/processes.js
+++ src/public/js/processes.js
@@ -1,5 +1,155 @@
+function validateProcessForm(form){
+    let validated = false;
+    form.on('submit', function (event) {
+        // process name field validation
+        const patternNameProcess = /[^a-zA-ZÀ-ÿ0-9\sñÑ]/;
+        const nameProcess = $('#processname');
+        const invalidFeedbackNameProcess = $('#invalid-feedback-processname');
+        nameProcess.removeClass('border-dark');
+        if (nameProcess.val().length === 0) {
+            nameProcess.removeClass('is-valid');
+            nameProcess.addClass('is-invalid');
+            invalidFeedbackNameProcess.text('Por favor, agregue un nombre');
+        }else if (patternNameProcess.test(nameProcess.val())){
+            nameProcess.removeClass('is-valid');
+            nameProcess.addClass('is-invalid');
+            invalidFeedbackNameProcess.text('El nombre del proceso debe contener únicamente letras');
+        }else if (nameProcess.val().length < 1 || nameProcess.val().length > 30){
+            nameProcess.removeClass('is-valid');
+            nameProcess.addClass('is-invalid');
+            invalidFeedbackNameProcess.text('El nombre del proceso debe contener entre 1 y 30 caracteres');
+        }else{
+            nameProcess.removeClass('is-invalid');
+            nameProcess.addClass('is-valid');
+        };
+
+        // process description field validation
+        const patternDescProcess = /[^a-zA-ZÀ-ÿ0-9\sñÑ]/;
+        const descProcess = $('#processdescription');
+        const invalidFeedbackDescProcess = $('#invalid-feedback-processdescription');
+        descProcess.removeClass('border-dark');
+        if (descProcess.val().length === 0) {
+            descProcess.removeClass('is-valid');
+            descProcess.addClass('is-invalid');
+            invalidFeedbackDescProcess.text('Por favor, agregue una descripción');
+        }else if (patternDescProcess.test(descProcess.val())){
+            descProcess.removeClass('is-valid');
+            descProcess.addClass('is-invalid');
+            invalidFeedbackDescProcess.text('La descripción del proceso debe contener únicamente letras');
+        }else if (descProcess.val().length < 1 || descProcess.val().length > 40){
+            descProcess.removeClass('is-valid');
+            descProcess.addClass('is-invalid');
+            invalidFeedbackDescProcess.text('La descripción del proceso debe contener entre 1 y 40 caracteres');
+        }else{
+            descProcess.removeClass('is-invalid');
+            descProcess.addClass('is-valid');
+        };
+
+        // validate that the order is sequential
+        let orderSelectsValues = [];
+        const orderSelects = $('.orderProcess');
+        orderSelects.each(function(){
+            orderSelectsValues.push($(this).val())
+        })
+        
+        let sequencial = [];
+        let selectsSorted = orderSelectsValues.sort();
+        let selectsLength = selectsSorted.length;
+        let selectsMin = selectsSorted[0];
+        if(selectsLength > 1){
+            if(selectsMin == 1){
+                sequencial.push(true);
+                for (let i = 0; i < selectsLength-1; i++) {
+                    if(selectsSorted[i+1] - selectsSorted[i] == 0){
+                        sequencial.push(true);
+                    }else if(selectsSorted[i+1] - selectsSorted[i] == 1 || selectsSorted[i+1] - selectsSorted[i] == -1){
+                        sequencial.push(true);
+                    }else{
+                        sequencial.push(false);
+                    }
+                }
+            }else{
+                sequencial.push(false);
+            }
+        }else{
+            if(selectsMin == 1){
+                sequencial.push(true);
+            }else{
+                sequencial.push(false);
+            }
+        }
+
+        // show which element of the sequence is wrong
+        const invalidFeedbackOrderProcess = $('.invalid-feedback-process-order');
+        if(sequencial.length > 0){
+            orderSelects.each(function(i){
+                $(this).removeClass('border-dark');
+                if(sequencial[i] == true){
+                    $(this).removeClass('is-invalid');
+                    $(this).addClass('is-valid');
+                }else{
+                    $(this).removeClass('is-valid');
+                    $(this).addClass('is-invalid');
+                    invalidFeedbackOrderProcess.eq(i).text('El orden de las áreas no es válido');
+                }
+            })
+        }
+
+        // area process validation
+        let areaSelectsValues = [];
+        const areaSelects = $('.areaProcess');
+        areaSelects.each(function(){
+            areaSelectsValues.push($(this).val())
+        })
+
+        let duplicates = [];
+        let indexDuplicates = [];
+        areaSelectsValues.filter(function(val, i){
+            if(areaSelectsValues.indexOf(val) !== i){
+                duplicates.push(true);
+                indexDuplicates.push(i);
+            }else{
+                duplicates.push(false);
+                indexDuplicates.push(i);
+            }
+        })
+
+        const invalidFeedbackAreaProcess = $('.invalid-feedback-process-area');
+        if(duplicates.length > 0){
+            areaSelects.each(function(i){
+                $(this).removeClass('border-dark');
+                if(duplicates[i] == false){
+                    $(this).removeClass('is-invalid');
+                    $(this).addClass('is-valid');
+                }else{
+                    $(this).removeClass('is-valid');
+                    $(this).addClass('is-invalid');
+                    invalidFeedbackAreaProcess.eq(i).text('Por favor, evite duplicar áreas');
+                }
+            })
+        }
+
+        // Checking the validations
+        const nonValidatedFields = $('.is-invalid');
+        if (nonValidatedFields.length !== 0){
+            validated = false;
+        }else{
+            validated = true;
+        }
+        
+        // avoid default form behavior
+        if(!validated){
+            event.preventDefault();
+            event.stopPropagation();
+        }
+    })
+}
+
 $(document).ready(function(){
+    // ------------------------------- List file -------------------------------
     const processTable = new DataTable('#processes-table', {
+        responsive: true,
+        fixedColumns: true,
         paging: false,
         info: false,
         padding: false,
@@ -7,11 +157,16 @@ $(document).ready(function(){
             {
                 "orderable": false,
                 "targets": 4
+            },
+            {
+                "width": "50%",
+                "targets": 1
             }
         ],
-        dom: '<"float-start pb-2"f><"button-add-process pb-2"B>', 
+        dom: "<'row pb-2'<'col-12 col-md-6 order-last order-md-first'<'float-start'f>><'col-12 col-md-6 order-first order-md-last'<'button-add-process'B>>>" +
+                "<'row'<'col-sm-12'tr>>",
         fnInitComplete: function(){
-            $('div.button-add-process').html('<a href="/processes/add" class="btn btn-outline-success border-success border-2 float-end" role="button"><i class="bi bi-bezier"></i> Añadir proceso</a>');
+            $('div.button-add-process').html('<a href="/processes/add" class="btn btn-outline-success border-success border-2 float-md-end mb-3 mb-md-0" role="button"><i class="bi bi-bezier"></i> Añadir proceso</a>');
         },
         language:{
             url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-MX.json',
@@ -111,4 +266,20 @@ $(document).ready(function(){
             })
         })
     })
+
+    // validate add role form
+    validateProcessForm($('#addProcessForm'));
+
+    // ------------------------------- Edit file -------------------------------
+    $('.remove-process-select').on('click', function(e){
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+        $(this).parents().eq(1).removeClass('py-3');
+        $(this).parents().eq(1).hide('slow', ()=>{
+            $(this).parents().eq(1).remove();
+        })
+    })
+
+    // validate add role form
+    validateProcessForm($('#editProcessForm'));
 })
--- src/routes/processes.js
+++ src/routes/processes.js
@@ -16,23 +16,58 @@ router.get('/add', isLoggedIn, async (req, res) => {
     res.render('processes/add', {areas: areas, orden: orden_posible});
 })
 
+router.post('/add', isLoggedIn, async (req, res) => {
+    const proceso = req.body;
+    
+    // almacenar nuevo proceso
+    const {insertId} = await pool.query('INSERT INTO Procesos (nombre) VALUES (?);', [proceso.processname]);
+
+    // organizar el orden de los procesos para su almacenamiento
+    let procesosOrden = [];
+    for (let i = 0; i < proceso.orderProcess.length; i++) {
+        procesosOrden.push([Number(proceso.areaProcess[i]), insertId, Number(proceso.orderProcess[i])]);
+    }
+
+    // almacenar los procesos en procesosOrdenes
+    await pool.query('INSERT INTO ProcesosOrdenes (area_id, proceso_id, orden) VALUES ?;', [procesosOrden]);
+
+    req.flash('success', 'El proceso <b>'+proceso.processname+'</b> ha sido creado correctamente');
+    res.redirect('/processes');
+})
+
 router.get('/', isLoggedIn, async (req, res) => {
     //const procesos =  await pool.query('SELECT Procesos.procesoId AS proceso_id, Procesos.nombre AS proceso_nombre, COUNT(ProcesosOrdenes.orden) AS total_areas, GROUP_CONCAT(ProcesosOrdenes.orden ORDER BY ProcesosOrdenes.orden) AS orden_areas, GROUP_CONCAT(Areas.nombre ORDER BY ProcesosOrdenes.orden ASC) AS areas, total_productos.total AS total_productos FROM Procesos INNER JOIN ProcesosOrdenes ON Procesos.procesoId = ProcesosOrdenes.proceso_id INNER JOIN Areas ON ProcesosOrdenes.area_id = Areas.areaId INNER JOIN (SELECT Procesos.procesoId AS proceso_id, COUNT(Productos.productoId) AS total FROM Procesos LEFT JOIN Productos ON Procesos.procesoId = Productos.proceso_id GROUP BY Procesos.procesoId) AS total_productos ON Procesos.procesoId = total_productos.proceso_id GROUP BY Procesos.procesoId;');
     const procesos =  await pool.query('SELECT Procesos.procesoId AS proceso_id, Procesos.nombre AS proceso_nombre, COUNT(ProcesosOrdenes.orden) AS total_areas, total_productos.total AS total_productos FROM Procesos INNER JOIN ProcesosOrdenes ON Procesos.procesoId = ProcesosOrdenes.proceso_id INNER JOIN Areas ON ProcesosOrdenes.area_id = Areas.areaId INNER JOIN (SELECT Procesos.procesoId AS proceso_id, COUNT(Productos.productoId) AS total FROM Procesos LEFT JOIN Productos ON Procesos.procesoId = Productos.proceso_id GROUP BY Procesos.procesoId) AS total_productos ON Procesos.procesoId = total_productos.proceso_id WHERE Procesos.nombre <> "Personalizado" GROUP BY Procesos.procesoId;');
-
     res.render('processes/list', {procesos: procesos});
 })
 
 router.post('/listStages', isLoggedIn, async (req, res) => {
     const procesoId = req.body.proccessId;
-    const etapas =  await pool.query('SELECT Procesos.nombre AS proceso_nombre, GROUP_CONCAT(ProcesosOrdenes.orden ORDER BY ProcesosOrdenes.orden) AS orden_areas, GROUP_CONCAT(Areas.nombre ORDER BY ProcesosOrdenes.orden ASC) AS areas FROM ((Procesos INNER JOIN ProcesosOrdenes ON Procesos.procesoId = ProcesosOrdenes.proceso_id INNER JOIN Areas ON ProcesosOrdenes.area_id = Areas.areaId)) WHERE Procesos.procesoId = ? GROUP BY Procesos.procesoId;', [procesoId]);
-    const proceso = await pool.query('SELECT nombre FROM Procesos WHERE procesoId = ?;', [procesoId])
+    const proceso =  await pool.query('SELECT Procesos.nombre AS proceso_nombre, GROUP_CONCAT(ProcesosOrdenes.orden ORDER BY ProcesosOrdenes.orden) AS orden_areas, GROUP_CONCAT(Areas.nombre ORDER BY ProcesosOrdenes.orden ASC) AS proceso_areas FROM ((Procesos INNER JOIN ProcesosOrdenes ON Procesos.procesoId = ProcesosOrdenes.proceso_id INNER JOIN Areas ON ProcesosOrdenes.area_id = Areas.areaId)) WHERE Procesos.procesoId = ? GROUP BY Procesos.procesoId;', [procesoId]);
+    const procesoNombre = await pool.query('SELECT nombre FROM Procesos WHERE procesoId = ?;', [procesoId])
+
+    // ajustar la estructura de los datos
+    let etapas = [];
+    let etapa = {};
+    const valoresOrden = Object.values(proceso[0].orden_areas.split(','));
+    const valoresAreas = Object.values(proceso[0].proceso_areas.split(','));
+    for (let i = 0; i < valoresOrden.length; i++) {
+        etapa = {
+            orden: valoresOrden[i],
+            area: valoresAreas[i],
+        }
+        etapas.push(etapa);
+        etapa = {};
+    }
+
+    // crear nuevo objeto con el proceso ya formateado
+    const procesoEstructurado = {
+        proceso_nombre: proceso[0].proceso_nombre,
+        etapas: etapas
+    }
     
-    // separar el orden de las areas y las areas
-    etapas[0].orden_areas = etapas[0].orden_areas.split(',');
-    etapas[0].areas = etapas[0].areas.split(',');
-    
-    res.send({etapas: etapas[0], proceso: proceso[0]});
+    //res.send({etapas: etapas[0], procesoNombre: procesoNombre[0]});
+    res.send({proceso: procesoEstructurado});
 })
 
 router.post('/listProducts', isLoggedIn, async (req, res) => {
@@ -40,6 +75,62 @@ router.post('/listProducts', isLoggedIn, async (req, res) => {
     const productos =  await pool.query('SELECT Productos.nombre AS producto_nombre FROM Procesos INNER JOIN Productos ON Procesos.procesoId = Productos.proceso_id WHERE Procesos.procesoId = ?;', [procesoId]);
     const proceso = await pool.query('SELECT nombre FROM Procesos WHERE procesoId = ?;', [procesoId]);
     res.send({productos: productos, proceso: proceso[0]});
+})
+
+router.get('/edit/:id', isLoggedIn, async (req, res) => {
+    const {id} = req.params;
+    const proceso =  await pool.query('SELECT Procesos.procesoId AS proceso_id, Procesos.nombre AS proceso_nombre, GROUP_CONCAT(ProcesosOrdenes.orden ORDER BY ProcesosOrdenes.orden) AS orden_areas, GROUP_CONCAT(Areas.nombre ORDER BY ProcesosOrdenes.orden ASC) AS proceso_areas FROM Procesos INNER JOIN ProcesosOrdenes ON Procesos.procesoId = ProcesosOrdenes.proceso_id INNER JOIN Areas ON ProcesosOrdenes.area_id = Areas.areaId WHERE Procesos.ProcesoId = ? GROUP BY Procesos.procesoId;', [id]);
+    const areas = await pool.query('SELECT areaId, nombre FROM Areas');
+
+    // ajustar la estructura de los datos
+    let etapas = [];
+    let etapa = {};
+    const valoresOrden = Object.values(proceso[0].orden_areas.split(','));
+    const valoresAreas = Object.values(proceso[0].proceso_areas.split(','));
+    for (let i = 0; i < valoresOrden.length; i++) {
+        etapa = {
+            orden: valoresOrden[i],
+            area: valoresAreas[i],
+        }
+        etapas.push(etapa);
+        etapa = {};
+    }
+
+    // crear nuevo objeto con el proceso ya formateado
+    const procesoEstructurado = {
+        proceso_id: proceso[0].proceso_id,
+        proceso_nombre: proceso[0].proceso_nombre,
+        etapas: etapas
+    }
+
+    // generar arreglo con una serie de acuerdo al numero de areas (orden del proceso)
+    let ordenPosible = [];
+    areas.forEach((_, i) => {
+        ordenPosible.push(i+1);
+    });
+
+    res.render('processes/edit', {proceso: procesoEstructurado, areas: areas, orden: ordenPosible});
+})
+
+router.post('/edit/:id', isLoggedIn, async (req, res)=>{
+    const {id} = req.params;
+    const proceso = req.body;
+    
+    // actualizar proceso
+    await pool.query('UPDATE Procesos SET nombre = ? WHERE procesoId = ?', [proceso.processname, id])
+
+    // eliminar y volver a añadir los permisos
+    let procesosOrden = [];
+    if (proceso !== undefined){
+        await pool.query('DELETE FROM ProcesosOrdenes WHERE proceso_id = ?', [id]);
+        for (let i = 0; i < proceso.orderProcess.length; i++) {
+            procesosOrden.push([Number(proceso.areaProcess[i]), id, Number(proceso.orderProcess[i])]);
+        }
+        await pool.query('INSERT INTO ProcesosOrdenes (area_id, proceso_id, orden) VALUES ?;', [procesosOrden]);
+    }
+
+    req.flash('success', 'El proceso <b>'+proceso.processname+'</b> ha sido editado correctamente');
+    res.redirect('/processes');
 })
 
 module.exports = router;
