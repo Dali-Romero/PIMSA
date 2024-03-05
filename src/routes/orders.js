3ae08af397a3396f774c5c4f79d96f9a2a7cc1f8
Hagantyr <kevinaherrerae@gmail.com>
2024-03-04 20:28:11 -0600

    [Feature] Revaluar ordenes (reasignar precio a los productos)

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const {Stream} = require('stream');
 const path = require('path');
 const pool = require('../database.js');
-const { createPdf } = require('../lib/helpers.js');
+const { dimensiones, moneda, createPdf } = require('../lib/helpers.js');
 const { isLoggedIn, IsAuthorized } = require('../lib/auth.js');
 
 const router = express.Router();
@@ -80,6 +80,141 @@ router.get('/download/:id', isLoggedIn, IsAuthorized('seeListOrders'), async (re
     res.attachment(`ot${id}_${timeMilis}.pdf`);
     res.contentType('application/pdf');
     readStream.pipe(res).on('error', function(e){console.error(e)});
+})
+
+router.get('/revalue/:id', isLoggedIn, IsAuthorized('editOrders'), async (req, res)=> {
+    const {id} = req.params;
+    const cotizacion = await pool.query('SELECT Ordenes.ordenId, Cotizaciones.*, Clientes.clienteId, Clientes.nombre, Clientes.descuento AS maxDescuento FROM Ordenes INNER JOIN Cotizaciones ON Ordenes.cot_id = Cotizaciones.cotId INNER JOIN Clientes ON Cotizaciones.cliente_id = Clientes.clienteId WHERE Ordenes.ordenId = ?;', [id]);
+    const productos = await pool.query('SELECT Productos.* FROM ((SELECT Ordenes.ordenId AS ordenId, ProductosCotizados.prodCotizadoId AS prodCotizadoId, Productos.nombre AS nombre, Productos.unidad AS unidad, Productos.precio AS precio, ProductosCotizados.cantidad AS cantidad, ProductosCotizados.acabados AS acabados, ProductosCotizados.archivo AS archivo, ProductosCotizados.largo AS largo, ProductosCotizados.ancho AS ancho, 0 AS fueraCatalogo FROM Ordenes INNER JOIN Cotizaciones ON Ordenes.cot_id = Cotizaciones.cotId INNER JOIN ProductosCotizados ON Cotizaciones.cotId = ProductosCotizados.cot_id INNER JOIN Productos ON ProductosCotizados.producto_id = Productos.productoId) UNION ALL (SELECT Ordenes.ordenId AS ordenId, FueraCatalogoCotizados.fueraCotizadoId AS prodCotizadoId, FueraCatalogoCotizados.concepto AS nombre, FueraCatalogoCotizados.unidad AS unidad, FueraCatalogoCotizados.precio AS precio, FueraCatalogoCotizados.cantidad AS cantidad, FueraCatalogoCotizados.acabados AS acabados, FueraCatalogoCotizados.archivo AS archivo, FueraCatalogoCotizados.largo AS largo, FueraCatalogoCotizados.ancho AS ancho, 1 AS fueraCatalogo FROM Ordenes INNER JOIN Cotizaciones ON Ordenes.cot_id = Cotizaciones.cotId INNER JOIN FueraCatalogoCotizados ON Cotizaciones.cotId = FueraCatalogoCotizados.cot_id)) AS Productos WHERE Productos.ordenId = ?;', [id]);
+    res.render('orders/revalue', {cotizacion: cotizacion[0], productos: productos});
+})
+
+router.post('/revalue/:id', isLoggedIn, IsAuthorized('editOrders'), async (req, res)=> {
+    const {id} = req.params;
+    const cotizacion = req.body.data.cotizacion;
+    const productos = req.body.data.productos;
+
+    // obtener id de la cotizacion relacionada con esta orden
+    const cotId = await pool.query('SELECT Cotizaciones.cotId AS cotId FROM Ordenes INNER JOIN Cotizaciones ON Ordenes.cot_id = Cotizaciones.cotId WHERE Ordenes.ordenId = ?;', [id]);
+
+    // ordenar cotizacion
+    const newCot = {
+        porcentajeDescuento: Number(cotizacion.porcentajeDescuento),
+        totalBruto: cotizacion.totalBruto,
+        descuento: cotizacion.descuento,
+        subtotal: cotizacion.subtotal,
+        iva: cotizacion.iva,
+        total: cotizacion.total
+    };
+
+    // actualizar cotizacion
+    await pool.query('UPDATE Cotizaciones SET ? WHERE cotId = ?', [newCot, cotId[0].cotId]);
+
+    // ordenar productos
+    let todosProductos = []
+    let newProducto = [];
+    productos.forEach(producto => {
+        newProducto = [
+            Number(producto.revalueproductId),
+            Number(producto.revaluepriceproduct),
+            producto.revaluepriceach,
+            producto.revalueamount,
+            producto.revalueisoutcatalog
+        ]
+        todosProductos.push(newProducto);
+        newProducto = [];
+    });
+
+
+    // agregar nuevos productos cotizados
+    if (todosProductos.length > 0){
+        for await (const producto of productos) {
+            if (producto.revalueisoutcatalog == '0') {
+                await pool.query('UPDATE ProductosCotizados SET  precio = ?, precioCadaUno = ?, monto = ? WHERE prodCotizadoId = ?;', [producto.revaluepriceproduct, producto.revaluepriceach, producto.revalueamount, producto.revalueproductId]);
+            } else {
+                await pool.query('UPDATE FueraCatalogoCotizados SET precio = ?, precioCadaUno = ?, monto = ? WHERE fueraCotizadoId = ?;', [producto.revaluepriceproduct, producto.revaluepriceach, producto.revalueamount, producto.revalueproductId]);
+            }
+        }
+    }
+
+    req.flash('success', 'La orden <b>OT-'+id+'</b> ha sido revaluada correctamente');
+    res.send({url: '/orders'});
+})
+
+router.post('/preview', isLoggedIn, async (req, res)=>{
+    const body = req.body.inputs;
+    const orderId = body.revalueorderid;
+    const cotizacion = await pool.query('SELECT Cotizaciones.* FROM Ordenes INNER JOIN Cotizaciones ON Ordenes.cot_id = Cotizaciones.cotId WHERE Ordenes.ordenId = ?;', [orderId]);
+    const empleado = await pool.query('SELECT Cotizaciones.empleado FROM Ordenes INNER JOIN Cotizaciones ON Ordenes.cot_id = Cotizaciones.cotId WHERE Ordenes.ordenId = ?;', [orderId]);
+    const cliente = await pool.query('SELECT Clientes.* FROM Ordenes INNER JOIN Cotizaciones ON Ordenes.cot_id = Cotizaciones.cotId INNER JOIN Clientes ON Cotizaciones.cliente_id = Clientes.clienteId WHERE Ordenes.ordenId = ?;', [orderId]);
+
+    // obtener informacion de los productos 
+    let i = 0;
+    const productos = {};
+    const valoresBody = Object.values(body);
+    for (const key in body) {
+        if (i>4) {
+            productos[key] = valoresBody[i];
+        }
+        i++;
+    }
+
+    // crear un arreglo con todos los productos
+    let j = 0;
+    let productosCot = {};
+    let productosCotArray = [];
+    const valoresProductos = Object.values(productos);
+    for (const key in productos) {
+        if (j !==0 && j%10 === 0) {
+            productosCotArray.push(productosCot);
+            productosCot = {};
+        }
+        productosCot[key.replace(/[0-9]/g, '')] = valoresProductos[j];
+        j++;
+    }
+    productosCotArray.push(productosCot);
+
+    // calculos de la cotizacion
+    let bruto = 0;
+    let subtotal = 0;
+    let descuento = 0;
+    let iva = 0;
+    productosCotArray.forEach(producto => {
+        if(producto.revalueunitproduct === 'Mt'){
+            producto.revaluemeasure = dimensiones(producto.revaluelengthproduct * producto.revaluewidthproduct);
+            producto.revaluepriceach = moneda(producto.revaluemeasure * producto.revaluepriceproduct);
+            producto.revalueamount = moneda(producto.revaluepriceach * producto.revaluequantityproduct);
+        }else{
+            producto.revaluemeasure = dimensiones(producto.revaluelengthproduct * producto.revaluewidthproduct);
+            producto.revaluepriceach = moneda(producto.revaluemeasure * producto.revaluepriceproduct);
+            producto.revalueamount = moneda(producto.revaluepriceproduct * producto.revaluequantityproduct);
+        }
+        bruto = bruto + producto.revalueamount;
+        bruto = moneda(bruto);
+    });
+    descuento = moneda((body.revaluenumdiscount*bruto)/100);
+    subtotal = moneda(bruto-descuento);
+    iva = moneda((16*subtotal)/100);
+    total = moneda(subtotal + iva);
+
+    // ajustar cotizacion
+    const cot = {
+        fecha: cotizacion[0].fecha,
+        cliente_id: cotizacion[0].cliente_id,
+        solicitante: cotizacion[0].solicitante,
+        proyecto: cotizacion[0].proyecto,
+        observaciones: cotizacion[0].observaciones,
+        empleado: empleado[0].empleado,
+        estatus: cotizacion[0].estatus,
+        totalBruto: bruto,
+        porcentajeDescuento: body.revaluenumdiscount,
+        descuento: descuento,
+        subtotal: subtotal,
+        iva: iva,
+        total:total
+    }
+
+    res.send({cotizacion:cot, cliente: cliente[0], productos: productosCotArray});
 })
 
 module.exports = router;
