3a707295ffb7296e954d63ee4298ade0ddda6966
Dali-Romero <odromero3@gmail.com>
2024-05-27 22:41:29 -0600

    Update users.js

--- src/routes/users.js
+++ src/routes/users.js
@@ -78,7 +78,7 @@ router.get('/edit/:id', isLoggedIn, IsAuthorized('editUsers'), async(req, res) =
         const empleados = await pool.query('SELECT empleadoId, nombreComp FROM Empleados WHERE empleadoId NOT IN (SELECT empleadoId FROM Usuarios INNER JOIN Empleados ON Usuarios.empleado_id = Empleados.empleadoId WHERE Usuarios.empleado_id <> 1000) OR empleadoId = ' + empleado[0] + ';');
         res.render('../views/users/editUser', {users: users[0], roles, empleados});
     } catch (error){
-        req.flash('error', "User: " + empleado);
+        req.flash('error', "User: " + empleado[0] + "\nError: " + error);
         res.redirect("/users");
     }
     
